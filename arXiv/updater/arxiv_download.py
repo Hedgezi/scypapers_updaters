933159e7b8420124a179fc0108c354ed29eed893
Mykola V <nikola@rootnix.net>
2023-08-06 13:11:46 +0200

    updated for correct usage of multithreading with ThreadPoolExecutor

--- arXiv/updater/arxiv_download.py
+++ arXiv/updater/arxiv_download.py
@@ -37,26 +37,26 @@ def download_paper(url: str, file_name: str):
 
 
 def download_all_entries_from_feed(feed: feedparser.FeedParserDict, previous_date: time.struct_time,
-                                   max_parallel_downloads: int = 10) -> list:
+                                   thread_pool_executor: ThreadPoolExecutor = ThreadPoolExecutor(max_workers=10)
+                                   ) -> list:
     """
     Download all papers from a given feed.
 
     :param feed: Feed to download papers from
     :param previous_date: Date, when DB was last updated
-    :param max_parallel_downloads: Maximum number of parallel downloads
+    :param thread_pool_executor: ThreadPoolExecutor for querying new downloads
     :return: List of downloads futures
     """
     download_futures = []
 
     for entry in feed.entries:
         if entry.updated_parsed < previous_date:
-            return download_futures
+            break
 
-        with ThreadPoolExecutor(max_workers=max_parallel_downloads) as executor:
-            cur_paper_id: str = entry.id.split('/')[-1]
-            download_futures.append(executor.submit(download_paper,
-                                                    "https://arxiv.org/e-print/{0}".format(cur_paper_id),
-                                                    cur_paper_id))
+        cur_paper_id: str = entry.id.split('/')[-1]
+        download_futures.append(thread_pool_executor.submit(download_paper,
+                                                            "https://arxiv.org/e-print/{0}".format(cur_paper_id),
+                                                            cur_paper_id))
 
     return download_futures
 
@@ -71,6 +71,7 @@ def get_all_previous_papers_from_api(previous_date: time.struct_time, category: 
     :param max_results: Maximum number of results per request
     :param max_parallel_downloads: Maximum number of parallel downloads
     """
+    thread_pool_executor = ThreadPoolExecutor(max_workers=max_parallel_downloads)
     download_futures = []
     request_iter = 0
 
@@ -80,14 +81,14 @@ def get_all_previous_papers_from_api(previous_date: time.struct_time, category: 
             'sortBy': 'lastUpdatedDate',
             'sortOrder': 'descending',
             'max_results': str(max_results),
-            'start': request_iter*max_results
+            'start': request_iter * max_results
         })
         if response.status_code != 200:
             print(f'Error: {response.status_code}')
             return
 
         new_download_futures = download_all_entries_from_feed(feedparser.parse(response.text), previous_date,
-                                                              max_parallel_downloads)
+                                                              thread_pool_executor)
         download_futures.extend(new_download_futures)
         if len(new_download_futures) < max_results:
             break
@@ -96,9 +97,10 @@ def get_all_previous_papers_from_api(previous_date: time.struct_time, category: 
 
     for future in download_futures:
         future.result()
+    thread_pool_executor.shutdown()
 
 
 if __name__ == '__main__':
-    time_to_test = (2023, 7, 21, 17, 0, 0, 0, 202, 0)
-    get_all_previous_papers_from_api(time.struct_time(time_to_test), 'cs.CV', MAX_ENTRIES_PER_REQUEST,
+    time_to_test = (2023, 8, 1, 10, 0, 0, 0, 213, 0)
+    get_all_previous_papers_from_api(time.struct_time(time_to_test), 'cs.AI', MAX_ENTRIES_PER_REQUEST,
                                      MAX_PARALLEL_DOWNLOADS)
