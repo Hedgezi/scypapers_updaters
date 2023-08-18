13bd271b3940704f8d3a34ea8f7f537aced5acad
Mykola V <nikola@rootnix.net>
2023-08-18 16:25:52 +0200

    added updater for pmc

--- pmc/updater.sh
+++ pmc/updater.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 sync s3://pmc-oa-opendata ./pmc-test/ --exclude "*" --include "/oa_comm/xml/all/"
+aws s3 sync s3://pmc-oa-opendata "$(realpath "$1")" --exclude "*" --include "/oa_comm/xml/all/"
