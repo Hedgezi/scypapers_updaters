579a1d740ac2491601d5a034103d37893bdb3672
Mykola V <nikola@rootnix.net>
2023-09-11 19:05:56 +0200

    acl downloader ready

--- acl/main.py
+++ acl/main.py
@@ -4,10 +4,13 @@ This script downloads the ACL Anthology corpus
 
 """
 import gzip
+import os
 
 import requests
 
 ACL_ANTHOLOGY_URL = "https://aclanthology.org/anthology.bib.gz"
+
+PATH_TO_ACL_ANTHOLOGY = "data/"
 
 
 def get_all_acl_entries() -> list[str]:
@@ -31,7 +34,7 @@ def get_all_acl_entries() -> list[str]:
                     break
                 if line_strip.startswith("url"):
                     urls.append(
-                        line_strip[line_strip.find('"') + 1 : line_strip.rfind('"')]
+                        line_strip[line_strip.find('\"') + 1 : line_strip.rfind('\"')]
                     )
         else:
             continue
@@ -39,5 +42,22 @@ def get_all_acl_entries() -> list[str]:
     return urls
 
 
+def download_acl_pdf(url: str, filename: str = "") -> None:
+    if not filename:
+        filename = url.split("/")[-1]
+    r = requests.get(url, timeout=90)
+    r.raise_for_status()
+    with open(PATH_TO_ACL_ANTHOLOGY + filename, "wb") as f:
+        f.write(r.content)
+
+
+def download_acl_anthology_corpus(urls: list[str]) -> None:
+    for link in urls:
+        filename = link.split("/")[-1] + ".pdf"
+        if filename in os.listdir(PATH_TO_ACL_ANTHOLOGY):
+            continue
+        download_acl_pdf(link + ".pdf", filename)
+
+
 if __name__ == "__main__":
-    get_all_acl_entries()
+    download_acl_anthology_corpus(get_all_acl_entries())
